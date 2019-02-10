141455f8b4ed6216b4858b60b5de1bcc191d801a
SashaKotsevich <kotsevich.a@yandex.ru>
2019-02-10 14:06:42 +0300

    add authntication

---
+++ src/Controller/ShopController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShopController extends AbstractController
+{
+    /**
+     * @Route("/homepage", name="app_homepage")
+     */
+    public function homepage()
+    {
+        $this->denyAccessUnlessGranted("IS_AUTHENTICATED_FULLY");
+        $user= $this->getUser();
+        return $this->render('shop/homepage.html.twig', [
+            'user_name' => $user->getUsername(),
+        ]);
+    }
+}
